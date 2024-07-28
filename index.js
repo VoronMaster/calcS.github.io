f45595163cff1af1eca293a834de599e40a86b59
VoronMaster <al3x.b0rn@yandex.ru>
2024-07-28 16:57:35 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-const startSum = parseInt(document.querySelector('#startSum').textContent);
-let sum = parseInt(document.querySelector('#sum').textContent);
 
-
+// let object1 =  {
+//     date1: '2024-06-10', 
+//     date2: '2024-07-17', 
+//     idSum: '#sum', 
+//     idStartSum: '#startSum', 
+//     proc: 0.05, 
+//     Ssum: 500
+// }
+function calc({date1, date2, idSum, idStartSum, proc, Ssum}) {
+const startSum = parseInt(document.querySelector(idStartSum).textContent);
+let sum = parseInt(document.querySelector(idSum).textContent);
 // Установка дат
-const loanDate = new Date('2024-06-10');
-const interestStartDate = new Date('2024-07-17');
+const loanDate = new Date(date1);
+const interestStartDate = new Date(date2);
 const currentDate = new Date();
 
 // Расчет количества дней между датами
 const daysDiff = Math.ceil((currentDate - interestStartDate) / (1000 * 60 * 60 * 24));
 
 // Расчет процентной ставки в день
-const dailyInterestRate = 0.05; // Например, 1% в день
+const dailyInterestRate = proc; // Например, 1% в день
 
 if (currentDate >= interestStartDate && currentDate >= loanDate) {
-    const loanAmount = 500; // Сумма займа
+    const loanAmount = Ssum; // Сумма займа
 
     // Расчет процентов за каждый день
     const interestAmount = loanAmount * dailyInterestRate * daysDiff;
@@ -23,7 +31,31 @@ if (currentDate >= interestStartDate && currentDate >= loanDate) {
     // console.log(`Проценты к оплате: ${interestAmount.toFixed(2)}`);
 } 
 
-document.querySelector('#sum').textContent = sum
+document.querySelector(idSum).textContent = sum
+console.log('ok')
+}
+
+calc( {
+    date1: '2024-06-10', 
+        date2: '2024-07-17', 
+        idSum: '#sum', 
+        idStartSum: '#startSum', 
+        proc: 0.05, 
+        Ssum: 500
+}
+    )
+
+    calc( {
+        date1: '2024-05-29', 
+            date2: '2024-07-06', 
+            idSum: '#sum-2', 
+            idStartSum: '#startSum2', 
+            proc: 0.05, 
+            Ssum: 700
+    }
+        )
+
+
 
 
 
